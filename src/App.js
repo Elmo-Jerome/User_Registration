fba2ad4e28b4c7f317446eadaccf7aeb458f320a
Elmo Jerome <elmojeromem@gmail.com>
2021-09-10 18:57:09 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { useState } from "react";
-import Axios from 'axios';
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [company, setCompany] = useState("");
-  const [role, setRole] = useState("");
-
-  const userRegister = () => {
-    Axios.post('http://localhost:3001/user-register', { name: name, email: email, company: company, role: role }).then(() => { 
-      alert("You are now registered!");
-    }).catch(() => {
-      alert("Try again.");
-    })
-  }
-
   return (
-    <div className="App form-group">
-      <label for="name">Name</label>
-      <input type="text" className="form-control" id="name" placeholder="Enter your Full name here" required onChange={(event) => { setName(event.target.value); }} />
-      <label for="email">Email</label>
-      <input type="email" className="form-control" id="email" placeholder="Enter your valid email here" required onChange={(event) => { setEmail(event.target.value); }} />
-      <label for="company">Company</label>
-      <input type="text" className="form-control" id="company" placeholder="Enter your Company name here" required onChange={(event) => { setCompany(event.target.value); }} />
-      <label for="role">Role</label>
-      <input type="text" className="form-control" id="role" placeholder="Enter your Company role here" required onChange={(event) => { setRole(event.target.value); }} />
-
-      <button className="form-control btn-info" onClick={userRegister}>Register</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
