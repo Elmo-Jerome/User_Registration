5a3b56ebc39dfa534ddc185655bce5d62206c566
Elmo Jerome <elmojeromem@gmail.com>
2021-09-12 05:16:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import { Route, Switch,Link } from 'react-router-dom';
-import {UserRegistration} from './UserRegistration';
-import {CreateUser} from './CreateUser';
-import {EditUser} from './EditUser';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <nav className="navbar bg-light navbar-expand-lg navbar-light">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <Link to="/" className="nav-link">Registered Users</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/createUser" className="nav-link">Register</Link>
-          </li>
-        </ul>
-      </nav>
-    <Switch>
-      <Route exact path="/" component={UserRegistration} />
-      <Route exact path="/edit/:id" component={EditUser} />
-      <Route exact path="/createUser" component={CreateUser} />
-    </Switch>
-    </div>
+    <div className="App"></div>
   );
 }
 
