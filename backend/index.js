0df4f8899fa87bbc0c22b4a6b72344fcc7ea35d3
Elmo Jerome <elmojeromem@gmail.com>
2021-09-13 18:18:21 +0800

    updated frontedn to backend connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const validator = require('validator');
 const UserInfoModel = require('./models/UserInfo');
 
-
+app.use(cors());
+app.use(express.json());
 
 /// Database connection
 
@@ -14,12 +16,18 @@ mongoose.connect(
 );
 
 
-app.get("/insertUser",async (req, res) =>{
+app.post("/adduser",async (req, res) =>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const company = req.body.company;
+    const role = req.body.role;
+
+    
     const userInfo  = new UserInfoModel({
-        name: "Rick", 
-        email: "Rick@mail.com", 
-        company: "RickandMorty", 
-        role: "Scientist"
+        name: name, 
+        email: email, 
+        company: company, 
+        role: role
     });
     await userInfo.save();
     res.send("User added");
@@ -28,9 +36,9 @@ app.get("/insertUser",async (req, res) =>{
 app.get("/UserList",async (req, res) =>{
     UserInfoModel.find({}, (err, result) => {
         if(err){
-            res.send(err)
+            res.send(err);
         } else{
-            res.send(result)
+            res.send(result);
         }
     })
 })
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 
 
 
 export const RegistrationForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [company, setCompany] = useState("");
+    const [role, setRole] = useState("");
+
+
+    const newUser = () => {
+        Axios.post('http://localhost:3001/adduser', { 
+            name: name, 
+            email: email, 
+            company: company, 
+            role: role 
+        }).then(() => {
+            alert(`New user: ${name} with ${email} from ${company} which is a ${role}`);
+        }).catch(() => {
+            alert("Try again.");
+        })
+    }
+
+
     return (
-        <div>Register here</div>
+        <form>
+            <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter your full name..."
+                    onChange={(event) => {
+                        setName(event.target.value);
+                    }}
+                />
+                <label htmlFor="email">Email:</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your valid email..."
+                    onChange={(event) => {
+                        setEmail(event.target.value);
+                    }}
+                />
+                <label htmlFor="company">Company:</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    name="company"
+                    id="company"
+                    placeholder="Enter your Company name..."
+                    onChange={(event) => {
+                        setCompany(event.target.value);
+                    }}
+                />
+                <label htmlFor="role">Role:</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    name="role"
+                    id="role"
+                    placeholder="Enter your company role..."
+                    onChange={(event) => {
+                        setRole(event.target.value);
+                    }}
+                />
+            </div>
+            <div className="form-group mt-4">
+                <button type="submit" className="btn btn-primary" onClick={newUser}>
+                    Register
+                </button>
+            </div>
+        </form>
     )
 }
